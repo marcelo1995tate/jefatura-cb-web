5466ba8dc3f2c21d3340b4bc9152b92e4a8786e3
Marcelo <Marcelo@DESKTOP-P76ICLL>
2022-07-18 20:22:49 -0300

    dasd

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
-const express = require("express");
-
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+    return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
+const express = require('express');
 const app = express();
-
+app.use(requireHTTPS);
 app.use(express.static("./dist/jefatura-cb-web"));
 
-app.get("/*", function (req, res) {
-  res.sendFile("index.html", { root: "dist/jefatura-cb-web" });
-});
-
-app.listen(process.env.PORT || 8080);
-
-console.log(`Running on port ${process.env.PORT || 8080}`);
+  app.get("/*", function(req, res) {
+    res.sendFile("index.html", {root: "dist/jefatura-cb-web/"}
+    );
+  });
+    app.listen(process.env.PORT || 8080);
