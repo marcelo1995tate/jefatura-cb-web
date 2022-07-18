46d506793f6da8d8889ce7c9cbdb2068773a7a1f
Marcelo <Marcelo@DESKTOP-P76ICLL>
2022-07-18 20:46:54 -0300

    dasd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {PlanificacionesComponent} from "./planificaciones/planificaciones.component";
+import {PlanosComponent} from "./planos/planos.component";
+import {LogosComponent} from "./logos/logos.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+
+  {path: 'planificaciones', component: PlanificacionesComponent},
+  {path: 'planos', component: PlanosComponent},
+  {path: 'logos', component: LogosComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './general/footer/footer.component';
 import { HeaderComponent } from './general/header/header.component';
+import { HomeComponent } from './home/home.component';
+import { PlanificacionesComponent } from './planificaciones/planificaciones.component';
+import { PlanosComponent } from './planos/planos.component';
+import { LogosComponent } from './logos/logos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    PlanificacionesComponent,
+    PlanosComponent,
+    LogosComponent
   ],
   imports: [
     BrowserModule,
