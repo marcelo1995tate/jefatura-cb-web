43a8d89f2882c97fc21492e95461c9857878bcdd
Marcelo <Marcelo@DESKTOP-P76ICLL>
2022-07-18 20:07:09 -0300

    dasd

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
-const path = require('path');
-const express = require('express');
+const express = require("express");
+
 const app = express();
 
-// Serve static files
-app.use(express.static(__dirname + '/dist/jefatura-cb-web'));
+app.use(express.static("./dist/jefatura-cb-web"));
 
-// Send all requests to index.html
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/jefatura-cb-web/index.html'));
+app.get("/*", function (req, res) {
+  res.sendFile("index.html", { root: "dist/jefatura-cb-web" });
 });
 
-// default Heroku port
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 8080);
+
+console.log(`Running on port ${process.env.PORT || 8080}`);
