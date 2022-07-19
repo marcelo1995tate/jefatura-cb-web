01ce25874bec5c81d208dc956821769a0c42c353
Marcelo <Marcelo@DESKTOP-P76ICLL>
2022-07-18 21:32:48 -0300

    dasd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,15 @@ import {HomeComponent} from "./home/home.component";
 import {PlanificacionesComponent} from "./planificaciones/planificaciones.component";
 import {PlanosComponent} from "./planos/planos.component";
 import {LogosComponent} from "./logos/logos.component";
+import {ModulosComponent} from "./modulos/modulos.component";
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-
+  {path: 'home', component: HomeComponent},
   {path: 'planificaciones', component: PlanificacionesComponent},
   {path: 'planos', component: PlanosComponent},
   {path: 'logos', component: LogosComponent},
-  {path: 'modulos', component: LogosComponent},
+  {path: 'modulos', component: ModulosComponent},
 
 ];
 
