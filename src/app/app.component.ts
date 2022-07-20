a496e5cbc5a54b9158db46b8fe139084893cb40a
Marcelo <Marcelo@DESKTOP-P76ICLL>
2022-07-20 01:10:59 -0300

    dasd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {DomSanitizer} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'jefatura-cb-web';
+  public cssUrl: string;
+  private dato;
+  private modoOscuro: string = '/assets/css/oscuro.css';
+  private modoDia: string = '/assets/css/dia.css';
+
+  constructor(public sanitizer: DomSanitizer) {
+    this.dato = localStorage.getItem('modoVision');
+
+    if (!this.dato){
+      this.cssUrl = this.modoDia;
+    }
+    else {
+      this.cssUrl =this.dato;
+    }
+  }
+
+  cambiarEstilos(temaSeleccionado: string) {
+    localStorage.removeItem('modoVision');
+    switch (temaSeleccionado) {
+      case 'dia':
+        this.cssUrl = this.modoDia;
+        break;
+      case 'oscuro':
+        this.cssUrl = this.modoOscuro;
+        break;
+      default:
+        this.cssUrl = this.modoDia;
+        break;
+    }
+    localStorage.setItem('modoVision', this.cssUrl);
+
+  }
 }
--- src/app/general/header/header.component.ts
+++ src/app/general/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AppComponent} from "../../app.component";
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  public dia: boolean = true;
 
-  constructor() { }
+  constructor(private serviceAppComponent: AppComponent) {
+    if (!localStorage.getItem('modoVision') || localStorage.getItem('modoVision') == "/assets/css/dia.css")
+      this.dia = true
+    else
+      this.dia = false
+  }
 
   ngOnInit(): void {
   }
 
+  public cargarTemaDia() {
+    this.dia = true;
+    this.serviceAppComponent.cambiarEstilos('dia');
+  }
+
+  public cargarTemaOscuro() {
+    this.dia = false;
+    this.serviceAppComponent.cambiarEstilos('oscuro');
+  }
 }
